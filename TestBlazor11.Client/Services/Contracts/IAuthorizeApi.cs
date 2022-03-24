b3c225c8eb3fc90a2337752937eae861cbc07557
okahara1 <y.okahara@syunwa.co.jp>
2022-03-24 10:14:42 +0900

    プロジェクト ファイルを追加します。

---
+++ TestBlazor11.Client/Services/Contracts/IAuthorizeApi.cs
@@ -0,0 +1,16 @@
+﻿using TestBlazor11.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBlazor11.Client.Services.Contracts
+{
+    public interface IAuthorizeApi
+    {
+        Task Login(LoginParameters loginParameters);
+        Task Register(RegisterParameters registerParameters);
+        Task Logout();
+        Task<UserInfo> GetUserInfo();
+    }
+}
